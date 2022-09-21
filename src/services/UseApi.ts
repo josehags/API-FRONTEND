45bfaf11100535cd867c5443d6af2f5ac300c4a8
José Henrique Guedes <henriquealvesguedes1912@gmail.com>
2022-09-21 10:44:10 -0300

    fixed untracked files

--- src/services/UseApi.ts
+++ src/services/UseApi.ts
@@ -1,34 +1,5 @@
 // //responsavel pelas requisições
 
-// import axios from 'axios';
-
-// const api = axios.create({
-//   baseURL: process.env.REACT_APP_API,
-// });
-
-// export const useApi = () => ({
-//   validateToken: async (token: string) => {
-//     const response = await api.post('/validate', { token });
-//     return response.data;
-//   },
-
-//   signin: async (username: string, password: string) => {
-//     //retornado resposta falsa antes da original
-//     return {
-//       user: { id: 3, name: 'José', username: 'jose@gmail.com' },
-//       token: '123456789',
-//     };
-//     // const response = await api.post('/signin', {
-//     //   username: username,
-//     //   password: password,
-//     // });
-//   },
-
-//   logout: async () => {
-//     const response = await api.post('/logout');
-//     return response.data;
-//   },
-// });
 import axios from 'axios';
 
 const api = axios.create({
